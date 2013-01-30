efcf047bf1c0b613358f0f8c68f38a252dd415c1
Chris Ruppel <chris.ruppel@gmail.com>
2013-01-29 23:07:27 -0600

    Refactor JS commands inside story object. Much more readable now.

--- js/main.js
+++ js/main.js
@@ -37,27 +37,44 @@
       "$('#s3 h2').toggleClass('whoosh'); ",
       "$('#s3 .sandbox, #s3 .sandbox2 .explain').toggleClass('hide'); ",
       "$('#s3 .translate').toggleClass('highlight'); ",
-      "$('#s3 .sandbox2 .de').toggleClass('t-x'); $('#s3 .x').toggleClass('red'); ",
-      "$('#s3 .sandbox2 .de').toggleClass('t-x').toggleClass('t-y'); $('#s3 .x').toggleClass('red'); $('#s3 .y').toggleClass('green'); ",
-      "$('#s3 .sandbox2 .de').toggleClass('t-y').toggleClass('t-z'); $('#s3 .y').toggleClass('green'); $('#s3 .z').toggleClass('blue'); ",
-      "$('#s3 .translate, #s3 .rotate').toggleClass('highlight'); $('#s3 .sandbox2 .de').toggleClass('t-z'); $('#s3 .z').toggleClass('blue'); ",
-      "$('#s3 .sandbox2 .de').toggleClass('r-x'); $('#s3 .x').toggleClass('red'); ",
-      "$('#s3 .sandbox2 .de').toggleClass('r-x').toggleClass('r-y'); $('#s3 .x').toggleClass('red'); $('#s3 .y').toggleClass('green'); ",
-      "$('#s3 .sandbox2 .de').toggleClass('r-y').toggleClass('r-z'); $('#s3 .y').toggleClass('green'); $('#s3 .z').toggleClass('blue'); ",
+      "$('#s3 .sandbox2 .de').toggleClass('t-x'); "+
+      "$('#s3 .x').toggleClass('red'); ",
+      "$('#s3 .sandbox2 .de').toggleClass('t-x').toggleClass('t-y'); "+
+      "$('#s3 .x').toggleClass('red'); "+
+      "$('#s3 .y').toggleClass('green'); ",
+      "$('#s3 .sandbox2 .de').toggleClass('t-y').toggleClass('t-z'); "+
+      "$('#s3 .y').toggleClass('green'); "+
+      "$('#s3 .z').toggleClass('blue'); ",
+      "$('#s3 .translate, #s3 .rotate').toggleClass('highlight'); "+
+      "$('#s3 .sandbox2 .de').toggleClass('t-z'); "+
+      "$('#s3 .z').toggleClass('blue'); ",
+      "$('#s3 .sandbox2 .de').toggleClass('r-x'); "+
+      "$('#s3 .x').toggleClass('red'); ",
+      "$('#s3 .sandbox2 .de').toggleClass('r-x').toggleClass('r-y'); "+
+      "$('#s3 .x').toggleClass('red'); "+
+      "$('#s3 .y').toggleClass('green'); ",
+      "$('#s3 .sandbox2 .de').toggleClass('r-y').toggleClass('r-z'); "+
+      "$('#s3 .y').toggleClass('green'); "+
+      "$('#s3 .z').toggleClass('blue'); ",
 
       // Slide 4
       "scrollTo('#s4'); ",
       "$('#s4 .cube').toggleClass('hide'); ",
       "$('#s4 .transform-style').toggleClass('hide'); ",
-      "$('#s4 .transform-style').toggleClass('dim').next().next().toggleClass('hide'); $('#s4 .sandbox').toggleClass('p-warp'); ",
-      "$('#s4 .perspective').toggleClass('dim').next().next().toggleClass('hide'); $('#s4 .sandbox').toggleClass('p-warp').toggleClass('po-warp'); ",
+      "$('#s4 .transform-style').toggleClass('dim').next().next().toggleClass('hide'); "+
+      "$('#s4 .sandbox').toggleClass('p-warp'); ",
+      "$('#s4 .perspective').toggleClass('dim').next().next().toggleClass('hide'); "+
+      "$('#s4 .sandbox').toggleClass('p-warp').toggleClass('po-warp'); ",
 
       // Slide 5
       "scrollTo('#s5'); ",
       "$('#s5 .cube').toggleClass('hide'); ",
-      "$('#s5 .translate').toggleClass('hide'); $('#s5 .sandbox').toggleClass('translate-demo'); ",
-      "$('#s5 .translate').toggleClass('dim').next().next().toggleClass('hide'); $('#s5 .sandbox').toggleClass('translate-demo').toggleClass('rotate-demo'); ",
-      "$('#s5 .rotate').toggleClass('dim').next().next().toggleClass('hide'); $('#s5 .sandbox').toggleClass('rotate-demo').toggleClass('backface-demo'); ",
+      "$('#s5 .translate').toggleClass('hide'); "+
+      "$('#s5 .sandbox').toggleClass('translate-demo'); ",
+      "$('#s5 .translate').toggleClass('dim').next().next().toggleClass('hide'); "+
+      "$('#s5 .sandbox').toggleClass('translate-demo').toggleClass('rotate-demo'); ",
+      "$('#s5 .rotate').toggleClass('dim').next().next().toggleClass('hide'); "+
+      "$('#s5 .sandbox').toggleClass('rotate-demo').toggleClass('backface-demo'); ",
       "$('#s5 .sandbox').toggleClass('backface-demo').toggleClass('backface-demo-cube'); ",
       "$('#s5 .sandbox').toggleClass('backface-demo-cube').toggleClass('backface-demo-cube-2sided'); ",
 
@@ -80,7 +97,8 @@
       "scrollTo('#s7'); ",
       "$('#s7 .slinky').toggleClass('hide'); ",
       "$('#s7 .slinky').toggleClass('extended'); ",
-      "$('#s7 p.slinky-desc').toggleClass('hide'); $('#s7 p.intro').toggleClass('dim'); ",
+      "$('#s7 p.slinky-desc').toggleClass('hide'); "+
+      "$('#s7 p.intro').toggleClass('dim'); ",
       "$('#s7 .slinky').toggleClass('move'); ",
       "$('.slinky, p.slinky-desc, p.solar-desc', '#s7').toggleClass('hide'); ",
       "$('#s7').toggleClass('space').find('.solar-system').toggleClass('hide'); ",
@@ -124,27 +142,44 @@
       "$('#s3 h2').toggleClass('whoosh'); ",
       "$('#s3 .sandbox, #s3 .sandbox2 .explain').toggleClass('hide'); ",
       "$('#s3 .translate').toggleClass('highlight'); ",
-      "$('#s3 .sandbox2 .de').toggleClass('t-x'); $('#s3 .x').toggleClass('red'); ",
-      "$('#s3 .sandbox2 .de').toggleClass('t-x').toggleClass('t-y'); $('#s3 .x').toggleClass('red'); $('#s3 .y').toggleClass('green'); ",
-      "$('#s3 .sandbox2 .de').toggleClass('t-y').toggleClass('t-z'); $('#s3 .y').toggleClass('green'); $('#s3 .z').toggleClass('blue'); ",
-      "$('#s3 .translate, #s3 .rotate').toggleClass('highlight'); $('#s3 .sandbox2 .de').toggleClass('t-z'); $('#s3 .z').toggleClass('blue'); ",
-      "$('#s3 .sandbox2 .de').toggleClass('r-x'); $('#s3 .x').toggleClass('red'); ",
-      "$('#s3 .sandbox2 .de').toggleClass('r-x').toggleClass('r-y'); $('#s3 .x').toggleClass('red'); $('#s3 .y').toggleClass('green'); ",
-      "$('#s3 .sandbox2 .de').toggleClass('r-y').toggleClass('r-z'); $('#s3 .y').toggleClass('green'); $('#s3 .z').toggleClass('blue'); ",
+      "$('#s3 .sandbox2 .de').toggleClass('t-x'); "+
+      "$('#s3 .x').toggleClass('red'); ",
+      "$('#s3 .sandbox2 .de').toggleClass('t-x').toggleClass('t-y'); "+
+      "$('#s3 .x').toggleClass('red'); "+
+      "$('#s3 .y').toggleClass('green'); ",
+      "$('#s3 .sandbox2 .de').toggleClass('t-y').toggleClass('t-z'); "+
+      "$('#s3 .y').toggleClass('green'); "+
+      "$('#s3 .z').toggleClass('blue'); ",
+      "$('#s3 .translate, #s3 .rotate').toggleClass('highlight'); "+
+      "$('#s3 .sandbox2 .de').toggleClass('t-z'); "+
+      "$('#s3 .z').toggleClass('blue'); ",
+      "$('#s3 .sandbox2 .de').toggleClass('r-x'); "+
+      "$('#s3 .x').toggleClass('red'); ",
+      "$('#s3 .sandbox2 .de').toggleClass('r-x').toggleClass('r-y'); "+
+      "$('#s3 .x').toggleClass('red'); "+
+      "$('#s3 .y').toggleClass('green'); ",
+      "$('#s3 .sandbox2 .de').toggleClass('r-y').toggleClass('r-z'); "+
+      "$('#s3 .y').toggleClass('green'); "+
+      "$('#s3 .z').toggleClass('blue'); ",
 
       // Slide 4
       "scrollTo('#s3'); ",
       "$('#s4 .cube').toggleClass('hide'); ",
       "$('#s4 .transform-style').toggleClass('hide'); ",
-      "$('#s4 .transform-style').toggleClass('dim').next().next().toggleClass('hide'); $('#s4 .sandbox').toggleClass('p-warp'); ",
-      "$('#s4 .perspective').toggleClass('dim').next().next().toggleClass('hide'); $('#s4 .sandbox').toggleClass('p-warp').toggleClass('po-warp'); ",
+      "$('#s4 .transform-style').toggleClass('dim').next().next().toggleClass('hide'); "+
+      "$('#s4 .sandbox').toggleClass('p-warp'); ",
+      "$('#s4 .perspective').toggleClass('dim').next().next().toggleClass('hide'); "+
+      "$('#s4 .sandbox').toggleClass('p-warp').toggleClass('po-warp'); ",
 
       // Slide 5
       "scrollTo('#s4'); ",
       "$('#s5 .cube').toggleClass('hide'); ",
-      "$('#s5 .translate').toggleClass('hide'); $('#s5 .sandbox').toggleClass('translate-demo'); ",
-      "$('#s5 .translate').toggleClass('dim').next().next().toggleClass('hide'); $('#s5 .sandbox').toggleClass('translate-demo').toggleClass('rotate-demo'); ",
-      "$('#s5 .rotate').toggleClass('dim').next().next().toggleClass('hide'); $('#s5 .sandbox').toggleClass('rotate-demo').toggleClass('backface-demo'); ",
+      "$('#s5 .translate').toggleClass('hide'); "+
+      "$('#s5 .sandbox').toggleClass('translate-demo'); ",
+      "$('#s5 .translate').toggleClass('dim').next().next().toggleClass('hide'); "+
+      "$('#s5 .sandbox').toggleClass('translate-demo').toggleClass('rotate-demo'); ",
+      "$('#s5 .rotate').toggleClass('dim').next().next().toggleClass('hide'); "+
+      "$('#s5 .sandbox').toggleClass('rotate-demo').toggleClass('backface-demo'); ",
       "$('#s5 .sandbox').toggleClass('backface-demo').toggleClass('backface-demo-cube'); ",
       "$('#s5 .sandbox').toggleClass('backface-demo-cube').toggleClass('backface-demo-cube-2sided'); ",
 
@@ -167,7 +202,8 @@
       "scrollTo('#s6'); ",
       "$('#s7 .slinky').toggleClass('hide'); ",
       "$('#s7 .slinky').toggleClass('extended'); ",
-      "$('#s7 p.slinky-desc').toggleClass('hide'); $('#s7 p.intro').toggleClass('dim'); ",
+      "$('#s7 p.slinky-desc').toggleClass('hide'); "+
+      "$('#s7 p.intro').toggleClass('dim'); ",
       "$('#s7 .slinky').toggleClass('move'); ",
       "$('.slinky, p.slinky-desc, p.solar-desc', '#s7').toggleClass('hide'); ",
       "$('#s7').toggleClass('space').find('.solar-system').toggleClass('hide'); ",
